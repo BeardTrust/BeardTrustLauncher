5dce51f3e7487ef083868f470956c8faaf6e4a3f
Matthew Crowell <>
2021-09-07 13:00:11 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser(description='Process command line arguments to determine which service to launch')
+parser.add_argument('--root-directory', '-r', dest='root_directory', help="specify the project's root directory")
+
+args = parser.parse_args()
+
+
+def main(arguments):
+    print("running 'mvn clean spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=dev'" +
+          " from args.root_directory")
+    os.chdir(args.root_directory)
+    subprocess.call('mvn clean spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=dev', shell=True)
+
+
+if __name__ == '__main__':
+    main(args)
